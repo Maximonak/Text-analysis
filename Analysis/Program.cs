adbf7b657acabddacd2851dd5e25b4c964b60559
User <User@WINDEV2303EVAL>
2023-04-10 14:52:09 -0700

    Add project files.

---
+++ Analysis/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string title;
+            title = Console.ReadLine();
+            string text;
+            text = Console.ReadLine();
+
+            Text firstText = new Text(title, text);
+            //Console.WriteLine(firstText.getText());
+            Console.WriteLine($"тип текста:{firstText.getType()}");
+        }
+
+        class Text
+        {
+            protected string title;
+            protected string text;
+
+
+            public Text(string title, string text) { this.title = title; this.text = text; }
+
+            public string Title
+            {
+                get
+                {
+                    return title;
+                }
+                set
+                {
+                    title = value;
+                }
+            }
+
+            public string Content
+            {
+                get
+                {
+                    return text;
+                }
+                set
+                {
+                    text = value;
+                }
+            }
+
+            public string getType()
+            {
+                Analysis analysis = new Analysis(text);
+                return analysis.analyze();
+            }
+
+            int getLength()
+            {
+                return text.Length;
+            }
+
+            class Analysis
+            {
+
+                private string text;
+                
+
+                public Analysis(string text) { this.text = text; }
+                public string analyze()
+                {
+                    string type;
+                    int length = text.Length;
+
+                    if (length < 10) { type = "Короткий"; } else { type = "Длинный"; }
+                    return type;
+                }
+            }
+        }
+
+    }
+}
